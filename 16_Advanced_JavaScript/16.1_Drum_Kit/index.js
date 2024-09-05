353fb74e255cfdf815b30aa7c142c0758e5e893b
kathrynwissinger-portfolio <kathrynwissinger.portfolio@gmail.com>
2024-09-04 20:25:25 -0500

    simon game

--- 16_Advanced_JavaScript/16.1_Drum_Kit/index.js
+++ 16_Advanced_JavaScript/16.1_Drum_Kit/index.js
@@ -30,7 +30,7 @@ function makeSound(key) {
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
         break;
-        
+    
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
